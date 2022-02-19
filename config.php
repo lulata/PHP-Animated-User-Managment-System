c86412af9987864224349e6792d6e84886fd0981
David Atanasoski <61380566+lulata@users.noreply.github.com>
2022-02-19 16:12:54 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,12 @@
 <?php
 
 $hostname = "localhost";
-$username = "akhfqkli_login_register_youtube";
-$password = "_RxF9!NO?mzv";
-$database = "akhfqkli_login_register_youtube";
+$username = "";
+$password = "";
+$database = "dbName";
 
 $conn = mysqli_connect($hostname, $username, $password, $database) or die("Database connection failed");
 
-$base_url = "https://code.akhfasoft.net/login-register-youtube/";
-$my_email = "code@akhfasoft.net";
+
 
 ?>
